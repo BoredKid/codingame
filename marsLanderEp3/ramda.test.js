a9f0de196a07026a7f53d45aed7e4281da0e4035
BoredKid <cisskane@gmail.com>
2024-01-31 21:51:08 +0100

    calcNextTurn in progress for mars lander

--- marsLanderEp3/ramda.test.js
+++ marsLanderEp3/ramda.test.js
@@ -1,4 +1,3 @@
-// PROBLEM CONSTS
 const ZONE_WIDTH = 7000;
 const ZONE_HEIGHT = 3000;
 const LANDING_ZONE_MIN_WIDTH = 1000;
@@ -30,21 +29,6 @@ const getNewAngle = (rotation, angle) => {
 }
 
 
-// GENETIC ALGO
-// GENETIC ALGO CONSTS
-const POP = 40; // doit etre divisible par 5
-const ADN = 4; // doit etre divisible par 2
-const GENES = [
-    { rotation: 0, thrust: 0 },
-    { rotation: 0, thrust: 4 },
-    { rotation: 70, thrust: 0 },
-    { rotation: 70, thrust: 4 },
-    { rotation: -70, thrust: 0 },
-    { rotation: -70, thrust: 4 },
-]
-const GENERATIONS = 200;
-
-// TODO: improve this function
 const calcNextTurn = ({ rotation, thrust }, X, Y, hSpeed, vSpeed, fuel, angle, power) => {
     const newPower = getNewPower(thrust, power);
     const newAngle = getNewAngle(rotation, angle);
@@ -57,36 +41,5 @@ const calcNextTurn = ({ rotation, thrust }, X, Y, hSpeed, vSpeed, fuel, angle, p
     return { newPower, newAngle, newFuel, newVSpeed: Math.round(newVSpeed), newHSpeed: Math.round(newHSpeed), newY, newX }
 }
 
-
-
-
-const surfaceN = parseInt(readline()); // the number of points used to draw the surface of Mars.
-let land = [];
-for (let i = 0; i < surfaceN; i++) {
-    var inputs = readline().split(' ');
-    const landX = parseInt(inputs[0]); // X coordinate of a surface point. (0 to 6999)
-    const landY = parseInt(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-    land[i] = { x: landX, y: landY }
-}
-
-// game loop
-while (true) {
-    var inputs = readline().split(' ');
-    const X = parseInt(inputs[0]);
-    const Y = parseInt(inputs[1]);
-    const hSpeed = parseInt(inputs[2]); // the horizontal speed (in m/s), can be negative.
-    const vSpeed = parseInt(inputs[3]); // the vertical speed (in m/s), can be negative.
-    const fuel = parseInt(inputs[4]); // the quantity of remaining fuel in liters.
-    const rotate = parseInt(inputs[5]); // the rotation angle in degrees (-90 to 90).
-    const power = parseInt(inputs[6]); // the thrust power (0 to 4).
-
-    // Write an action using console.log()
-    // To debug: console.error('Debug messages...');
-
-
-    // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
-    console.log('-20 3');
-}
-
-
-
+console.log(calcNextTurn({ rotation: -45, thrust: 4 }, 5000, 2500, -50, 0, 1000, 90, 0))
+console.log(calcNextTurn({ rotation: -45, thrust: 4 }, 4950, 2498, -51, -3, 999, 75, 1))
